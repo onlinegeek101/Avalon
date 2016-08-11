3f5e8e1be7c436875c272c9dff4285639bb40142
Pavlu <pavlu@amazon.com>
2016-08-11 19:00:11 -0400

    Adding public folder.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.set('port', (process.env.PORT || 3000));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static('public'));
 
 /*io.on('connection', function(socket){
   console.log("New Connection: " + socket);
