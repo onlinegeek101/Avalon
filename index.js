50de96a801a43e2dd0322d7130af11d8137c921a
Pavlu <pavlu@amazon.com>
2016-08-11 19:05:20 -0400

    Readding Connect New User Logic

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function(req, res){
@@ -10,12 +11,12 @@ app.get('/', function(req, res){
 
 app.use(express.static('public'));
 
-/*io.on('connection', function(socket){
+io.on('connection', function(socket){
   console.log("New Connection: " + socket);
   socket.on('new message', function(msg){
     io.emit('new message', msg);
   });
-});*/
+});
 
 http.listen(app.get('port'), '0.0.0.0', function() {
   console.log('listening on *:' + app.get('port'));
