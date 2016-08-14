3eeda60b6b85edae19e5f0db444a0723c2f136c6
Pavlu <pavlu@amazon.com>
2016-08-14 17:42:09 -0400

    Only Using Capitals for Room Code.

--- roomcodeGenerator.js
+++ roomcodeGenerator.js
@@ -1,6 +1,6 @@
 module.exports = function () {
   var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";//abcdefghijklmnopqrstuvwxyz0123456789";
 
   for( var i=0; i < 5; i++ )
       text += possible.charAt(Math.floor(Math.random() * possible.length));
