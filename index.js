304e3f36cace75ce31fe69446baa35cb6baae405
Pavlu <pavlu@amazon.com>
2016-08-13 21:02:02 -0400

    Moving default to Join.html

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var randomColor = require('randomcolor');
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/join.html');
 });
 
 app.get('/host', function(req, res){
